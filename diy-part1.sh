2105cf25a1edef7c05add27c0dbade98d475c37f
gzmike <70585869+gzmike@users.noreply.github.com>
2020-10-17 11:07:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,3 @@
-git clone https://github.com/kenzok8/small.git
+#git clone https://github.com/kenzok8/small.git
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
